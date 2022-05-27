fbdbcf877c4eae0215945adfdd2e44ad6dca39c2
migiburgos <67326096+migiburgos@users.noreply.github.com>
2022-05-27 18:11:45 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+const Stack = createNativeStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+};
+
+const App = () => {
+  const [loaded] = useFonts({
+    interBold: require('./assets/fonts/Inter-Bold.ttf'),
+    interSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    interMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    interRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    interLight: require('./assets/fonts/Inter-Light.ttf'),
+  });
+
+  if (!loaded) return null;
+
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
